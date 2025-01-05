bbc2070770cceb9d411f9137554f561afa37d694
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-04 16:54:50 -0800

    correct notification icons

--- pages/contacts.py
+++ pages/contacts.py
@@ -15,20 +15,21 @@ def contacts_page(): # Contacts page
     with ui.row().style('justify-content: center; align-items: center; width: 100%; height: 50vh;'): # Center the contacts title
         inject_lottie() # inject lottie animation
         ui.html(f'''<lottie-player src="{lottie_url}" loop autoplay style="height: 200px;"></lottie-player>''') # play file
-        with ui.column().classes('about-me').style('max-width: 600px;'): # Adjust padding and column width
-            ui.label('Contact Me').style('font-size: 32px; color: white; text-align: center;') # Mail title
+        with ui.column().classes('about-me').style('max-width: 600px; text-align: center'): # Adjust padding and column width
+            ui.label('Contact Me').style('font-size: 32px; color: white;') # Mail title
             # Create input fields for the form
             name = ui.input('Name').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')    
             email = ui.input('Email').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')
             message = ui.textarea('Message').props('label-color=white clearable input-class=text-white rows=13').style('font-size: 16px; width: 100%; height: 300px;')
             ui.button('Send', on_click=lambda: submit_form(name.value, email.value, message.value)).style('color: white; width: 100%;') # submit button
+            ui.label('vuvictor@csu.fullerton.edu').style('font-size: 20px; color: white;') # Mail title
     vu_footer() # footer function
 
 def submit_form(name, email, message): # Submit form
     if name and email and message: # Check if fields are not empty   
-        ui.notify('Thank you. Your message has been sent.', position='center', color='green') # notify message
+        ui.notify('Thank you. Your message has been sent.', position='center', type='positive') # notify message
     else: 
-        ui.notify('Please fill in all fields.', position='center', color='red') # notify error
+        ui.notify('Please fill in all fields.', position='center', type='negative') # notify error
 
 @ui.page('/contacts') # Route to contacts page
 def contacts():
