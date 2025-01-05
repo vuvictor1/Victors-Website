db8c46f1c2570e9a16eab3a32fe622a3962ec373
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-04 16:48:18 -0800

    contact updates

--- pages/contacts.py
+++ pages/contacts.py
@@ -12,23 +12,23 @@ def contacts_page(): # Contacts page
     vu_header() # header function
     inject_ui() # inject CSS effects
 
-    with ui.row().style('display: flex; justify-content: center; align-items: center; width: 100%; height: 60vh;'): # Center the contacts title
+    with ui.row().style('justify-content: center; align-items: center; width: 100%; height: 50vh;'): # Center the contacts title
         inject_lottie() # inject lottie animation
-        ui.html(f'''<lottie-player src="{lottie_url}" loop autoplay style="height: 200px; display: block; margin: 0 auto;"></lottie-player>''') # play file
-        with ui.column().classes('about-me').style('width: 100%; max-width: 600px; padding: 20px;'): # Adjust padding and column width
-            ui.label('Contact Us').style('font-size: 32px; color: white; text-align: center; display: block; margin: 20px auto;')
+        ui.html(f'''<lottie-player src="{lottie_url}" loop autoplay style="height: 200px;"></lottie-player>''') # play file
+        with ui.column().classes('about-me').style('max-width: 600px;'): # Adjust padding and column width
+            ui.label('Contact Me').style('font-size: 32px; color: white; text-align: center;') # Mail title
             # Create input fields for the form
-            name = ui.input('Name').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')
+            name = ui.input('Name').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')    
             email = ui.input('Email').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')
-            message = ui.textarea('Message').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%; height: 100px;')
-            ui.button('Send', on_click=lambda: submit_form(name.value, email.value, message.value)).style('font-size: 16px; color: white; width: 100%;') # submit button
+            message = ui.textarea('Message').props('label-color=white clearable input-class=text-white rows=13').style('font-size: 16px; width: 100%; height: 300px;')
+            ui.button('Send', on_click=lambda: submit_form(name.value, email.value, message.value)).style('color: white; width: 100%;') # submit button
     vu_footer() # footer function
 
 def submit_form(name, email, message): # Submit form
-    if name and email and message: # Check if fields are not empty
-        ui.notify('Thank you. Your message has been sent.', position='bottom', color='green') # notify message
+    if name and email and message: # Check if fields are not empty   
+        ui.notify('Thank you. Your message has been sent.', position='center', color='green') # notify message
     else: 
-        ui.notify('Please fill in all fields.', position='bottom', color='red') # notify error
+        ui.notify('Please fill in all fields.', position='center', color='red') # notify error
 
 @ui.page('/contacts') # Route to contacts page
 def contacts():
