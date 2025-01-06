ccb18b5cc836813647bcd04d3dd0dfcf0ca8b0ca
Victor V. Vu <78053016+vuvictor1@users.noreply.github.com>
2025-01-05 20:13:51 -0800

    form completed and modernized

--- pages/contacts.py
+++ pages/contacts.py
@@ -6,30 +6,37 @@
 from nicegui import ui
 from components import vu_header, vu_footer, inject_ui, inject_lottie
 
+# Colors to reuse
+background_color = "#3B3B3B"
+card_background_color = "#2C2C2C"
+text_color = "white"
+
 lottie_url = 'https://lottie.host/d1f5a60c-d57a-405a-b1ce-6dcf4b933d07/5prfVzNgqF.json' # lottie animation
 
 def contacts_page(): # Contacts page
     vu_header() # header function
     inject_ui() # inject CSS effects
 
-    with ui.row().style('justify-content: center; width: 100%; margin-top: 20px;'): # Center the contacts title
+    with ui.row().style(f'justify-content: center; width: 100%; margin-top: 20px; background-color: {background_color};'): # Center the contacts title
         inject_lottie() # inject lottie animation
-        with ui.column().classes('work').style('align-items: center;'): # Column for the contacts
+        with ui.column().classes('work').style(f'align-items: center; background-color: {card_background_color}; padding: 20px; border-radius: 10px;'): # Column for the contacts
             ui.html(f'''<lottie-player src="{lottie_url}" loop autoplay style="height: 200px;"></lottie-player>''') # play file
-            ui.label('Contact Me').style('font-size: 32px; color: white;') # Mail title
+            ui.label('Contact Me').style(f'font-size: 32px; color: {text_color};') # Mail title
             # Create input fields for the form
-            name = ui.input('Name').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')    
-            email = ui.input('Email').props('label-color=white clearable input-class=text-white').style('font-size: 16px; width: 100%;')
-            message = ui.textarea('Message').props('label-color=white clearable input-class=text-white rows=13').style('font-size: 16px; width: 100%; height: 300px;')
-            ui.button('Send', on_click=lambda: submit_form(name.value, email.value, message.value)).style('color: white; width: 50%;') # submit button
-            ui.label('vuvictor@csu.fullerton.edu').style('font-size: 20px; color: white;') # mail 
+            ui.html(f'''
+                <form action="https://api.web3forms.com/submit" method="POST" style="width: 100%;">
+                    <input type="hidden" name="access_key" value="5378da98-377b-464f-990d-f97e70c28023">
+                    <input type="text" name="name" placeholder="Your Name" required style="font-size: 16px; width: 100%; margin-bottom: 10px; background-color: {background_color}; color: {text_color}; border: none; padding: 10px; border-radius: 5px;">
+                    <input type="email" name="email" placeholder="Your Email (So I can contact you)" required style="font-size: 16px; width: 100%; margin-bottom: 10px; background-color: {background_color}; color: {text_color}; border: none; padding: 10px; border-radius: 5px;">
+                    <textarea name="message" placeholder="Your Message" required style="font-size: 16px; width: 100%; height: 300px; margin-bottom: 10px; background-color: {background_color}; color: {text_color}; border: none; padding: 10px; border-radius: 5px;"></textarea>
+                    <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
+                    <div style="text-align: center;">
+                        <button type="submit" style="color: {text_color}; width: 25%; background-color: #5898D4; padding: 10px 20px; cursor: pointer; border-radius: 5px; font-size: 20px;">Send</button>
+                    </div>
+                </form>
+            ''')
+            ui.label('📬 vuvictorgeek@gmail.com').style(f'font-size: 20px; color: {text_color};') # mail label
     vu_footer() # footer function
-
-def submit_form(name, email, message): # Submit form
-    if name and email and message: # Check if fields are not empty   
-        ui.notify('Your message has been sent.', position='center', type='positive') # notify message
-    else: # If fields are empty
-        ui.notify('Missing fields.', position='center', type='negative') # notify error
 
 @ui.page('/contacts') # Route to contacts page
 def contacts():
